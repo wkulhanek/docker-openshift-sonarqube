95c1ab4e63a87dd7b0bfe6084e82ca5c7064b112
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-06-18 14:24:49 -0400

    Updated for Sonarqube 7.2

--- build7.sh
+++ build7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export VERSION=7.1
+export VERSION=7.2
 docker build -f Dockerfile7 . -t wkulhanek/sonarqube:${VERSION}
 docker tag wkulhanek/sonarqube:${VERSION} wkulhanek/sonarqube:latest
 docker push wkulhanek/sonarqube:${VERSION}
