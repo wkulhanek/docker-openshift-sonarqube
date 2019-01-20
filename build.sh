51836f400bcf4f9dbffe9aea3e159d78566b84b4
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-01-20 11:15:10 +0000

    Updated to Sonarqube 6.7.6

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export VERSION=6.7.5
+export VERSION=6.7.6
 docker build . -t docker.io/wkulhanek/sonarqube:${VERSION}
 #docker tag docker.io/wkulhanek/sonarqube:${VERSION} docker.io/wkulhanek/sonarqube:latest
 docker push docker.io/wkulhanek/sonarqube:${VERSION}
